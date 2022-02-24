0a62babd55e3238df2dddeae2dd3ad7bf4c2072e
Ieuan Skinner <ieuanskinner@gmail.com>
2022-02-24 22:11:06 +0000

    Update seed to remove a lot of boiler plate incl. excessive instance variables

--- db/seeds/09_fm_as_unit_2.rb
+++ db/seeds/09_fm_as_unit_2.rb
@@ -1,11 +1,10 @@
-@fm_as_u2_branch = @a_level_further_maths_mind_map.branches.create!(name: 'FM AS U2', label: 'AS Unit 2: Further Statistics', topic_area: @statistics, position: 'l')
+fm_as_u2_branch = @a_level_further_maths_mind_map.branches.create!(name: 'FM AS U2', label: 'AS Unit 2: Further Statistics', topic_area: @statistics, position: 'l')
 
-@fm2_2_1_branch = @fm_as_u2_branch.child_branches.create!(name: 'FM2.2.1', label: 'FM2.2.1 Random Variables and the Poisson Process')
-@fm2_2_1a_branch = @fm2_2_1_branch.child_branches.create!(name: 'FM2.2.1a', label: 'FM2.2.1a Meath and Variance of Independent Random Variables')
-@fm2_2_1b_branch = @fm2_2_1_branch.child_branches.create!(name: 'FM2.2.1b', label: 'FM2.2.1b Discrete Probability Distributions')
-@fm2_2_1c_branch = @fm2_2_1_branch.child_branches.create!(name: 'FM2.2.1c', label: 'FM2.2.1c Continuous Probability Distributions')
-@fm2_2_1d_branch = @fm2_2_1_branch.child_branches.create!(name: 'FM2.2.1d', label: 'FM2.2.1d Poisson and exponential distributions')
+fm2_2_1_branch = fm_as_u2_branch.child_branches.create!(name: 'FM2.2.1', label: 'FM2.2.1 Random Variables and the Poisson Process')
+fm2_2_1_branch.child_branches.create!(name: 'FM2.2.1a', label: 'FM2.2.1a Meath and Variance of Independent Random Variables')
+fm2_2_1_branch.child_branches.create!(name: 'FM2.2.1b', label: 'FM2.2.1b Discrete Probability Distributions')
+fm2_2_1_branch.child_branches.create!(name: 'FM2.2.1c', label: 'FM2.2.1c Continuous Probability Distributions')
+fm2_2_1_branch.child_branches.create!(name: 'FM2.2.1d', label: 'FM2.2.1d Poisson and exponential distributions')
 
-@fm2_2_2_branch = @fm_as_u2_branch.child_branches.create!(name: 'FM2.2.2', label: 'FM2.2.2 Exploring relationships between variables and goodness fit of a model')
-@fm2_2_2a_branch = @fm2_2_2_branch.child_branches.create!(name: 'FM2.2.2a', label: 'FM2.2.2a Correlation & Goodness of fit')
-
+fm2_2_2_branch = fm_as_u2_branch.child_branches.create!(name: 'FM2.2.2', label: 'FM2.2.2 Exploring relationships between variables and goodness fit of a model')
+fm2_2_2_branch.child_branches.create!(name: 'FM2.2.2a', label: 'FM2.2.2a Correlation & Goodness of fit')
--- db/seeds/13_fm_a2_unit_6.rb
+++ db/seeds/13_fm_a2_unit_6.rb
@@ -1,18 +1,18 @@
-@fm_a2_u6_branch = @a_level_further_maths_mind_map.branches.create!(name: 'FM A2 U6', label: 'A2 Unit 6: Further Mechanics', topic_area: @mechanics, position: 'r')
+fm_a2_u6_branch = @a_level_further_maths_mind_map.branches.create!(name: 'FM A2 U6', label: 'A2 Unit 6: Further Mechanics', topic_area: @mechanics, position: 'r')
 
-@fm2_6_1_branch = @fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.1', label: 'FM2.6.1 Rectilinear motion')
-@fm2_6_1a_branch = @fm2_6_1_branch.child_branches.create!(name: 'FM2.6.1a', label: 'FM2.6.1a Rectilinear calculus')
+fm2_6_1_branch = fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.1', label: 'FM2.6.1 Rectilinear motion')
+fm2_6_1_branch.child_branches.create!(name: 'FM2.6.1a', label: 'FM2.6.1a Rectilinear calculus')
 
-@fm2_6_2_branch = @fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.2', label: 'FM2.6.2 Momentum and Impulse')
-@fm2_6_2a_branch = @fm2_6_2_branch.child_branches.create!(name: 'FM2.6.2a', label: 'FM2.6.2a Momentum - vectors')
+fm2_6_2_branch = fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.2', label: 'FM2.6.2 Momentum and Impulse')
+fm2_6_2_branch.child_branches.create!(name: 'FM2.6.2a', label: 'FM2.6.2a Momentum - vectors')
 
-@fm2_6_3_branch = @fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.3', label: 'FM2.6.3 Moments and Centre of Mass')
-@fm2_6_3a_branch = @fm2_6_3_branch.child_branches.create!(name: 'FM2.6.3a', label: 'FM2.6.3a Centre of mass - particles/uniform lamina')
-@fm2_6_3b_branch = @fm2_6_3_branch.child_branches.create!(name: 'FM2.6.3b', label: 'FM2.6.3b Centre of mass - rigid bodies')
+fm2_6_3_branch = fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.3', label: 'FM2.6.3 Moments and Centre of Mass')
+fm2_6_3_branch.child_branches.create!(name: 'FM2.6.3a', label: 'FM2.6.3a Centre of mass - particles/uniform lamina')
+fm2_6_3_branch.child_branches.create!(name: 'FM2.6.3b', label: 'FM2.6.3b Centre of mass - rigid bodies')
 
-@fm2_6_4_branch = @fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.4', label: 'FM2.6.4 Equilibrium of Rigid Bodies')
-@fm2_6_4a_branch = @fm2_6_4_branch.child_branches.create!(name: 'FM2.6.4a', label: 'FM2.6.4a Equilibrium of Rigid Bodies')
+fm2_6_4_branch = fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.4', label: 'FM2.6.4 Equilibrium of Rigid Bodies')
+fm2_6_4_branch.child_branches.create!(name: 'FM2.6.4a', label: 'FM2.6.4a Equilibrium of Rigid Bodies')
 
-@fm2_6_5_branch = @fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.5', label: 'FM2.6.5 Differential Equations')
-@fm2_6_5a_branch = @fm2_6_5_branch.child_branches.create!(name: 'FM2.6.5a', label: 'FM2.6.5a Kinematic modelling')
-@fm2_6_5b_branch = @fm2_6_5_branch.child_branches.create!(name: 'FM2.6.5b', label: 'FM2.6.5b Simple Harmonic Motion')
+fm2_6_5_branch = fm_a2_u6_branch.child_branches.create!(name: 'FM2.6.5', label: 'FM2.6.5 Differential Equations')
+fm2_6_5_branch.child_branches.create!(name: 'FM2.6.5a', label: 'FM2.6.5a Kinematic modelling')
+fm2_6_5_branch.child_branches.create!(name: 'FM2.6.5b', label: 'FM2.6.5b Simple Harmonic Motion')
