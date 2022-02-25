c66dbfe8fbf204829a2cd4f6c56e4cfef4ca82d6
Ieuan Skinner <ieuanskinner@gmail.com>
2022-02-25 20:30:42 +0000

    Added all links for M A2 U4 B branches

---
+++ db/seeds/20_m_a2_u4_b_links.rb
@@ -0,0 +1,30 @@
+create_link('M2.4.4a', 'M2.4.8a', 'resolving')
+create_link('M2.4.4a', 'M2.4.9a', 'resolving')
+
+create_link('M2.4.5a', 'FM2.6.1a', 'construct differential equations')
+create_link('M2.4.5a', 'M2.4.6a', 'use of')
+
+create_link('M2.4.6a', 'FM2.6.1a', 'solve differential equations')
+create_link('M2.4.6a', 'FM2.6.5a', 'extends to')
+
+create_link('M2.4.7a', 'M2.4.10a', 'units')
+
+create_link('M2.4.8a', 'M2.4.8b', 'vector kinematics')
+
+create_link('M2.4.8b', 'FM2.3.4a', '2-D vector calculus')
+create_link('M2.4.8b', 'FM2.3.4a', 'extends to 3-D')
+create_link('M2.4.8b', 'FM2.6.5a', 'kinematics calculus')
+
+create_link('M2.4.9a', 'FM2.3.2a', 'forces on an inclined plane')
+create_link('M2.4.9a', 'FM2.3.2a', 'resolving forces')
+create_link('M2.4.9a', 'FM2.6.1a', 'extends to')
+create_link('M2.4.9a', 'FM2.3.4a', 'forces')
+create_link('M2.4.9a', 'FM2.6.4a', 'forces in equilibrium')
+create_link('M2.4.9a', 'FM2.3.3b', 'resolve forces')
+create_link('M2.4.9a', 'M2.4.10a', 'identify forces')
+
+create_link('M2.4.10a', 'FM2.6.3a', 'moments')
+create_link('M2.4.10a', 'FM2.6.4a', 'moments')
+
+create_link('M2.4.11a', 'FM2.1.5a', '3-D vectors')
+create_link('M2.4.11a', 'FM2.3.4a', 'vectors in 3-D')
