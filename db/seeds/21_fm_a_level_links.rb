aa94d4513c5c62e62f98da3f4908ddae45c046df
Ieuan Skinner <ieuanskinner@gmail.com>
2022-02-25 21:59:39 +0000

    Added all links for Further Maths A Level

---
+++ db/seeds/21_fm_a_level_links.rb
@@ -0,0 +1,52 @@
+create_link('FM2.1.1a', 'FM2.4.1a', 'for proof of de Moivre')
+create_link('FM2.1.1a', 'FM2.1.4b', 'use of inducion')
+
+create_link('FM2.1.2', 'FM2.4.1a', 'basic complex numbers')
+
+create_link('FM2.1.2a', 'FM2.1.2b', 'basics')
+create_link('FM2.1.2a', 'FM2.4.4b', 'factorise x^2 + a^2')
+
+create_link('FM2.1.2b', 'FM2.1.2c', 'complex geometry')
+create_link('FM2.1.2b', 'FM2.4.6a', 'similarity')
+
+create_link('FM2.1.3a', 'FM2.1.3b', 'matrices basics')
+create_link('FM2.1.3a', 'FM2.4.3a', 'matrices basics')
+
+create_link('FM2.2.2a', 'FM2.5.3a', 'ranking')
+
+create_link('FM2.3.1a', 'FM2.6.2a', 'extend to 2-D')
+create_link('FM2.3.1a', 'FM2.3.4a', 'momentum & impulse')
+create_link('FM2.3.1a', 'FM2.3.2a', 'conservation of momentum')
+
+create_link('FM2.3.2a', 'FM2.6.5b', 'force in elastic strings & springs')
+
+create_link('FM2.3.3b', 'FM2.3.3c', 'extends to')
+
+create_link('FM2.3.4a', 'FM2.6.2a', 'momentum - vectors')
+create_link('FM2.3.4a', 'FM2.6.5', 'vector kinematics')
+
+create_link('FM2.4.1a', 'FM2.4.7a', 'relationship with trig functions')
+create_link('FM2.4.1a', 'FM2.4.8b', 'trig & exponential relationship')
+
+create_link('FM2.4.3a', 'FM2.4.3b', 'inverse')
+
+create_link('FM2.4.4b', 'FM2.4.5d', 'use of')
+
+create_link('FM2.4.5e', 'FM2.4.7b', 'inverse trig functions')
+
+create_link('FM2.4.5f', 'FM2.4.7b', 'inverse trig functions')
+
+create_link('FM2.4.6a', 'FM2.4.6b', 'basics')
+
+create_link('FM2.4.7a', 'FM2.4.7b', 'basics')
+
+create_link('FM2.4.8a', 'FM2.6.5a', 'use of')
+
+create_link('FM2.4.8b', 'FM2.6.5a', 'use of')
+
+create_link('FM2.6.3a', 'FM2.6.3b', 'techniques')
+create_link('FM2.6.3a', 'FM2.6.4a', 'use of Centre of Mass')
+
+create_link('FM2.6.3b', 'FM2.6.4a', 'use of Centre of Mass')
+
+create_link('FM2.6.5a', 'FM2.6.5b', 'use of')
