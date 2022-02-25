9d9fe3123fbfb979ef1d059fec9c1e47c6f16244
Ieuan Skinner <ieuanskinner@gmail.com>
2022-02-25 19:55:23 +0000

    Added all links for M AS U2 B branches

---
+++ db/seeds/17_m_as_u2_b_links.rb
@@ -0,0 +1,21 @@
+create_link('M2.2.6a', 'M2.4.7a', 'extends to')
+
+create_link('M2.2.7b', 'FM2.3.3a', 'suvat')
+create_link('M2.2.7b', 'FM2.3.4a', 'suvat')
+create_link('M2.2.7b', 'M2.4.9a', 'suvat')
+
+create_link('M2.2.7c', 'FM2.3.4a', 'kinematic calculus')
+create_link('M2.2.7c', 'M2.4.8b', 'kinematic calculus')
+
+create_link('M2.2.8a', 'FM2.3.1a', "Newton's 2nd law")
+create_link('M2.2.8a', 'FM2.6.1', 'N2L')
+create_link('M2.2.8a', 'FM2.3.4a', "force & Newton's laws")
+create_link('M2.2.8a', 'M2.4.10a', 'forces')
+create_link('M2.2.8a', 'M2.4.9a', 'extends to')
+create_link('M2.2.8a', 'M2.4.11a', 'forces')
+create_link('M2.2.8a', 'M2.2.9a', 'forces')
+
+create_link('M2.2.9a', 'M2.4.4a', 'vectors')
+create_link('M2.2.9a', 'M2.4.8a', 'vectors')
+create_link('M2.2.9a', 'M2.4.11a', 'extend to 3-D')
+create_link('M2.2.9a', 'FM2.6.2a', 'vectors')
