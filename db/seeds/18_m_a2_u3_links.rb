6992294b8fe6999bae9b4e46edb3e870bf3b5643
Ieuan Skinner <ieuanskinner@gmail.com>
2022-02-25 20:18:13 +0000

    Added all links for M A2 U3 branches

---
+++ db/seeds/18_m_a2_u3_links.rb
@@ -0,0 +1,67 @@
+create_link('M2.3.1a', 'FM2.1.1', 'developing sophistication')
+
+create_link('M2.3.2a', 'M2.3.2e', 'extends to')
+
+create_link('M2.3.2b', 'M2.3.4a', 'range of values')
+create_link('M2.3.2b', 'M2.3.4c', 'allowable r')
+
+create_link('M2.3.2c', 'M2.3.5b', 'domain & range')
+
+create_link('M2.3.2d', 'FM2.1.2c', 'link to transformations')
+create_link('M2.3.2d', 'FM2.1.3b', 'link to transformations')
+
+create_link('M2.3.2e', 'FM2.1.4b', 'splitting rational functions')
+create_link('M2.3.2e', 'FM2.4.4b', 'extends to')
+create_link('M2.3.2e', 'M2.3.7e', 'solving')
+
+create_link('M2.3.4a', 'FM2.1.1', '∑ notation')
+create_link('M2.3.4a', 'FM2.4.4a', 'binomial')
+
+create_link('M2.3.4b', 'FM2.1.4b', '∑ar + d')
+create_link('M2.3.4b', 'M2.3.4d', 'use in')
+
+create_link('M2.3.4c', 'M2.3.4d', 'use in')
+create_link('M2.3.4c', 'FM2.4.5a', 'to infinity')
+
+create_link('M2.3.5a', 'FM2.1.2b', 'radians')
+create_link('M2.3.5a', 'FM2.4.5f', 'radians')
+create_link('M2.3.5a', 'M2.3.7a', 'radians')
+create_link('M2.3.5a', 'M2.3.6c', 'radians')
+
+create_link('M2.3.5b', 'FM2.4.7a', 'sec, cot, cosec')
+create_link('M2.3.5b', 'M2.3.6c', 'sec, cot, cosec')
+
+create_link('M2.3.5c', 'FM2.1.2b', 'use of')
+create_link('M2.3.5c', 'M2.3.5d', 'use of formulae')
+create_link('M2.3.5c', 'FM2.4.2a', 'compound angle formulae')
+
+create_link('M2.3.5d', 'FM2.4.7a', 'extends to')
+
+create_link('M2.3.6a', 'FM2.6.1a', 'points of inflection')
+
+create_link('M2.3.6b', 'M2.3.6d', 'for proofs')
+create_link('M2.3.6b', 'M2.3.6c', 'applications')
+create_link('M2.3.6b', 'M2.3.7d', 'chain & product rules')
+create_link('M2.3.6b', 'M2.3.6e', 'for proofs')
+create_link('M2.3.6b', 'M2.3.8b', 'denominator for Newton Raphson')
+
+create_link('M2.3.6c', 'FM2.4.4a', 'trig, e, ln')
+create_link('M2.3.6c', 'FM2.4.5e', 'trig functions')
+create_link('M2.3.6c', 'M2.3.6e', 'use of')
+create_link('M2.3.6c', 'FM2.4.7b', 'trig functions e^x ln')
+create_link('M2.3.6c', 'M2.3.6d', 'use of')
+create_link('M2.3.6c', 'M2.3.7a', 'leads to')
+create_link('M2.3.6c', 'M2.3.8b', 'denominator for Newton Raphson')
+
+create_link('M2.3.6f', 'M2.3.7f', 'formation')
+
+create_link('M2.3.7a', 'FM2.4.5a', 'integration techniques')
+create_link('M2.3.7a', 'FM2.4.5b', 'integration techniques')
+create_link('M2.3.7a', 'FM2.4.5c', 'integration techniques')
+create_link('M2.3.7a', 'FM2.4.6b', 'integration')
+create_link('M2.3.7a', 'FM2.4.7b', 'trig functions e^x, ln')
+create_link('M2.3.7a', 'FM2.4.8a', 'integration techniques')
+create_link('M2.3.7a', 'M2.3.8c', 'area under curve')
+create_link('M2.3.7a', 'M2.3.7f', 'integration techniques')
+
+create_link('M2.3.7f', 'FM2.4.8a', 'extends to')
