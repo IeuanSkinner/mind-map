955de757ae84cbc1837a4930d5eadfa8093815b2
Ieuan Skinner <ieuanskinner@gmail.com>
2022-03-02 18:07:39 +0000

    Mind-map skeletons

--- app/javascript/packs/mind-map.js
+++ app/javascript/packs/mind-map.js
@@ -1,6 +1,6 @@
 import * as d3 from "d3";
 
-const mind_maps = document.querySelectorAll("[data-mind-map]");
+const mind_maps = [...document.querySelectorAll("[data-branches]")];
 const padding = 0;
 const width = window.innerWidth - padding;
 const height = window.innerHeight - padding;
@@ -10,68 +10,61 @@ const svg = d3
   .attr("width", width)
   .attr("height", height);
 
-// mind_maps.forEach((m) => {
-const data = JSON.parse(mind_maps[1].dataset.mindMap);
-console.log(data);
-const left_branches = {
-  ...data,
-  children: data.children.filter((branch) => branch.position === "l"),
+const add_branches = (branches, left, root) => {
+  branches
+    .selectAll("g")
+    .data(root.links())
+    .join("path")
+    .attr("stroke", (d) => d.target.data.colour)
+    .attr(
+      "d",
+      d3
+        .linkHorizontal()
+        .x((d) => (left ? -d.y : d.y))
+        .y((d) => d.x)
+    );
 };
-const right_branches = {
-  ...data,
-  children: data.children.filter((branch) => branch.position === "r"),
-};
 
-console.log(left_branches);
-console.log(right_branches);
+mind_maps.forEach((m, i) => {
+  const branch_data = JSON.parse(m.dataset.branches);
+  console.log(i);
+  console.log(branch_data);
 
-// const root = d3.hierarchy(data);
+  const left_branches = {
+    ...branch_data,
+    children: branch_data.children.filter((branch) => branch.position === "l"),
+  };
+  const right_branches = {
+    ...branch_data,
+    children: branch_data.children.filter((branch) => branch.position === "r"),
+  };
 
-const left_root = d3.hierarchy(left_branches);
-const right_root = d3.hierarchy(right_branches);
+  const left_root = d3.hierarchy(left_branches);
+  const right_root = d3.hierarchy(right_branches);
 
-// Compute the layout
-const dx = 15;
-const dy = 75;
-d3.tree().nodeSize([dx, dy])(left_root);
-d3.tree().nodeSize([dx, dy])(right_root);
+  // Compute the layout
+  const dx = 15;
+  const dy = 75;
+  d3.tree().nodeSize([dx, dy])(left_root);
+  d3.tree().nodeSize([dx, dy])(right_root);
 
-svg.attr("viewBox", [-(width / 2), -(height / 1.5), width, height * 1.5]);
+  svg.attr("viewBox", [-(width / 2), -(height / 1.5), width, height * 1.5]);
 
-const mind_map = svg.append("g").attr("width", "33%").attr("height", height);
+  const mind_map = svg
+    .append("g")
+    .attr("width", "33%")
+    .attr("height", height)
+    .attr("transform", `translate(${(i - 1) * 750}, 0)`);
 
-const branches = mind_map
-  .append("g")
-  .attr("fill", "none")
-  .attr("stroke", "black")
-  .attr("stroke-opacity", 1)
-  .attr("stroke-linecap", "round")
-  .attr("stroke-linejoin", "round")
-  .attr("stroke-width", 2);
+  const branches = mind_map
+    .append("g")
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .attr("stroke-opacity", 1)
+    .attr("stroke-linecap", "round")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-width", 2);
 
-branches
-  .selectAll("g")
-  .data(left_root.links())
-  .join("path")
-  .attr("stroke", (d) => d.target.data.colour)
-  .attr(
-    "d",
-    d3
-      .linkHorizontal()
-      .x((d) => -d.y)
-      .y((d) => d.x)
-  );
-
-branches
-  .selectAll("g")
-  .data(right_root.links())
-  .join("path")
-  .attr("stroke", (d) => d.target.data.colour)
-  .attr(
-    "d",
-    d3
-      .linkHorizontal()
-      .x((d) => d.y)
-      .y((d) => d.x)
-  );
-// });
+  add_branches(branches, true, left_root);
+  add_branches(branches, false, right_root);
+});
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_16_202523) do
     t.bigint "parent_branch_id"
     t.string "name", default: "", null: false
     t.string "label", default: "", null: false
-    t.string "colour", default: "#000", null: false
+    t.string "colour", default: "", null: false
     t.bigint "topic_area_id"
     t.string "position", default: "", null: false
     t.integer "hours", default: 0, null: false
