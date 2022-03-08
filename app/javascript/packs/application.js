408c8910f0778dbaf88e56fafa46a33b09ac8a75
Ieuan Skinner <ieuanskinner@gmail.com>
2022-03-08 19:00:32 +0000

    Better layout algorithm has been devised

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,65 +11,30 @@ Turbolinks.start();
 ActiveStorage.start();
 
 class App {
-  constructor(width, height, gap = 300) {
+  constructor(width, height, gap = 100) {
     this.width = width;
     this.height = height;
     this.gap = gap;
-    this.mind_maps = [];
-    this.$data = this.fetchData();
+    this.data = this.fetchData();
+    this.count = this.data.length;
     this.$svg = d3
       .select("svg#visualization")
       .attr("width", width)
       .attr("height", height);
 
-    this.build();
-    this.position();
-    this.fitViewBox();
+    this.availableWidth = this.width - this.gap * (this.count - 1);
+    this.maxWidth = this.availableWidth / this.count;
+
+    this.mindMaps = [];
+    this.data.forEach((data, i) =>
+      this.mindMaps.push(new MindMap(this, data, i))
+    );
   }
 
   fetchData() {
     return [...document.querySelectorAll("[data-mind-map]")].map(($el) =>
       JSON.parse($el.dataset.mindMap)
     ); // fetch data from DOM || localStorage || remote?
-  }
-
-  build() {
-    this.mind_maps = this.$data.map((data) => new MindMap(this, data));
-  }
-
-  position() {
-    let buffer = 0;
-
-    this.mind_maps.forEach((current, i) => {
-      const previous = this.mind_maps[i - 1];
-
-      if (previous) {
-        buffer +=
-          previous.width() / previous.sides +
-          this.gap +
-          current.width() / current.sides;
-
-        current.$mind_map.attr("transform", `translate(${buffer}, 0)`);
-      }
-    });
-  }
-
-  fitViewBox() {
-    const padding = 100;
-    let width = 0;
-    let height = 0;
-
-    this.mind_maps.forEach((mind_map, i) => {
-      width += mind_map.relativeWidth() + (i > 0 ? this.gap : 0);
-
-      const mind_map_height = mind_map.relativeHeight();
-      if (mind_map_height > height) height = mind_map_height;
-    });
-
-    width += 2 * padding;
-    height += 2 * padding;
-
-    this.$svg.attr("viewBox", [-padding, -(height / 2), width, height]);
   }
 }
 
--- app/javascript/packs/components/mind-map.js
+++ app/javascript/packs/components/mind-map.js
@@ -1,59 +1,86 @@
 import * as d3 from "d3";
 
 export default class MindMap {
-  constructor(app, data, dx = 15, dy = 75) {
+  constructor(app, data, index) {
     this.app = app;
     this.data = data;
-    this.dx = dx;
-    this.dy = dy;
-    this.$mind_map = this.app.$svg.append("g");
+    this.index = index;
     this.sides = 0;
+    // Use this.data to construct the MindMap's left/right trees.
+    this.tree("l");
+    this.tree("r");
 
-    this.build("l");
-    this.build("r");
+    this.priorMindMap = index > 0 ? this.app.mindMaps[index - 1] : null;
+    this.x = this.priorMindMap
+      ? this.priorMindMap.x + this.priorMindMap.width + this.app.gap
+      : 0;
+    this.width = this.app.maxWidth;
+    this.height = this.app.height;
+
+    this.$mindMap = this.app.$svg
+      .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height)
+      .attr("x", this.x);
+
+    this.draw("l");
+    this.draw("r");
   }
 
-  build(position) {
+  draw(position) {
+    if (!this.hasChildren(position)) return;
+
     this.hierarchy(position);
     this.layout(position);
-    this.draw_branches(position);
+    this.branchSvg(position);
+    this.drawBranches(position);
   }
 
   tree(position) {
-    return {
+    const children = this.data.children.filter((d) => d.position === position);
+    this[`${position}Tree`] = {
       ...this.data,
-      children: this.data.children.filter((d) => d.position === position),
+      children: children,
     };
+    if (children.length > 0) this.sides++;
+  }
+
+  hasChildren(position) {
+    return this[`${position}Tree`].children.length > 0;
   }
 
   hierarchy(position) {
-    this[`${position}_tree`] = this.tree(position);
-    if (this[`${position}_tree`].children.length > 0) this.sides++;
-    this[`${position}_hierarchy`] = d3.hierarchy(this[`${position}_tree`]);
+    this[`${position}Hierarchy`] = d3.hierarchy(this[`${position}Tree`]);
   }
 
   layout(position) {
-    d3.tree().nodeSize([this.dx, this.dy])(this[`${position}_hierarchy`]);
+    // d3.tree().nodeSize([15, 75])(this[`${position}Hierarchy`]);
+    d3.tree().size([this.height, this.width / 2])(this[`${position}Hierarchy`]);
   }
 
-  draw_branches(position) {
-    this.branch_config()
+  drawBranches(position) {
+    this[`$${position}BranchSvg`]
       .selectAll("g")
-      .data(this[`${position}_hierarchy`].links())
+      .data(this[`${position}Hierarchy`].links())
       .join("path")
       .attr("stroke", (d) => d.target.data.colour)
       .attr(
         "d",
         d3
           .linkHorizontal()
-          .x((d) => (position === "l" ? -d.y : d.y))
-          .y((d) => d.x)
+          .x((d) => (position === "l" ? d.y : d.y))
+          .y((d) => (!d.depth ? this.height / 2 : d.x)) // Always place the root node at half screen-height.
       );
   }
 
-  branch_config() {
-    return this.$mind_map
-      .append("g")
+  branchSvg(position) {
+    this[`$${position}BranchSvg`] = this.$mindMap
+      .append("svg")
+      .attr("width", this.width / 2)
+      .attr("height", this.height)
+      .attr("x", this.width / 2)
+      .attr("transform", `${position === "l" ? "scale(-1, 1)" : ""}`)
+      .attr("transform-origin", "center")
       .attr("fill", "none")
       .attr("stroke", "black")
       .attr("stroke-opacity", 1)
@@ -62,27 +89,20 @@ export default class MindMap {
       .attr("stroke-width", 2);
   }
 
-  bbox() {
-    return this.$mind_map.node().getBBox();
+  addGridPoint(position, x, y) {
+    this[`$${position}BranchSvg`]
+      .append("circle")
+      .attr("r", 1)
+      .attr("cx", x)
+      .attr("cy", y);
   }
 
-  boundingClientRect() {
-    return this.$mind_map.node().getBoundingClientRect();
-  }
-
-  width() {
-    return this.bbox().width;
-  }
-
-  height() {
-    return this.bbox().height;
-  }
-
-  relativeWidth() {
-    return this.boundingClientRect().width;
-  }
-
-  relativeHeight() {
-    return this.boundingClientRect().height;
+  drawGrid() {
+    for (let i = 0; i < this.width; i = i + 10) {
+      for (let j = 0; j < this.height; j = j + 10) {
+        this.addGridPoint("l", i, j);
+        this.addGridPoint("r", i, j);
+      }
+    }
   }
 }
