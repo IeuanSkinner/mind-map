af23f311fcf56939fc7e7a8517ad9fde82eb0663
Ieuan Skinner <ieuanskinner@gmail.com>
2022-06-05 18:15:34 +0100

    Added bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import Rails from "@rails/ujs";
+import * as bootstrap from "bootstrap";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
