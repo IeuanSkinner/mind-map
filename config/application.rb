4e90756c13b17de863a7d6602a49d148d9fd74d9
Ieuan Skinner <ieuanskinner@gmail.com>
2022-08-11 18:26:35 +0100

    Removed module declaration

--- config/application.rb
+++ config/application.rb
@@ -6,25 +6,23 @@ require "rails/all"
 # you've limited to :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MindMapTool
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+class Application < Rails::Application
+  # Initialize configuration defaults for originally generated Rails version.
+  config.load_defaults 7.0
 
-    config.before_configuration do
-      env_file = File.join(Rails.root, '.env')
-      File.foreach(env_file).each do |line|
-        env_var = line.split('=')
-        ENV[env_var.first] = env_var.last
-      end
+  config.before_configuration do
+    env_file = File.join(Rails.root, '.env')
+    File.foreach(env_file).each do |line|
+      env_var = line.split('=')
+      ENV[env_var.first] = env_var.last
     end
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
+
+  # Configuration for the application, engines, and railties goes here.
+  #
+  # These settings can be overridden in specific environments using the files
+  # in config/environments, which are processed later.
+  #
+  # config.time_zone = "Central Time (US & Canada)"
+  # config.eager_load_paths << Rails.root.join("extras")
 end
