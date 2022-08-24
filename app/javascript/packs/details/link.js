1c6fa8c7ec75715f03cb7bbd87ddb1c43f9c98f8
Ieuan Skinner <ieuanskinner@gmail.com>
2022-08-24 20:15:31 +0100

    Added in the basic functionality for the link details panel.

--- app/javascript/packs/details/link.js
+++ app/javascript/packs/details/link.js
@@ -3,13 +3,40 @@ import BaseDetails from "./base";
 export default class LinkDetails extends BaseDetails {
   constructor(app) {
     super(app, "link-details");
+
+    this.$sourceNode = this.$el.querySelector(".source-node");
+    this.$targetNode = this.$el.querySelector(".target-node");
+    this.$list = this.$el.querySelector(".list ul");
   }
 
   show(link) {
     const data = link.data;
-    
+
+    this.setTitle(data.label);
     this.setColour(data.colour);
 
+    this.setNode(this.$sourceNode, true, link.fromNode);
+    this.setNode(this.$targetNode, false, link.toNode);
+
+    this.setList(link);
+
     super.show();
+  }
+
+  setNode($node, isSource, node) {
+    $node.innerHTML = `The ${isSource ? "source" : "target"} of this link is ${node.label.data.label}.`;
+  }
+
+  setList(link) {
+    const data = link.data;
+
+    [...this.$list.children].forEach(child => child.remove());
+
+    data.label.split(";").forEach(concept => {
+      const $listItem = document.createElement("li");
+      $listItem.innerHTML = concept;
+
+      this.$list.append($listItem);
+    })
   }
 }
